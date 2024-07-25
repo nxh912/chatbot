5e6e51ed69de13e53b1cf82bc9332e72a31d3fb6
Ngai Lam Ho <nxh912@gmail.com>
2024-07-26 02:27:50 +0800

    adding server code

---
+++ backend/LangSmith.py
@@ -0,0 +1,23 @@
+from langchain_openai import ChatOpenAI
+
+#llm = ChatOpenAI()
+llm = ChatOpenAI(api_key="sk-proj-bVPN0I5jQf1fOxBj9FceT3BlbkFJAk96OECGUMTUTRCU2wR6")
+llm.invoke("how can langsmith help with testing?")
+
+'''
+
+
+import getpass
+import os
+
+#os.environ["OPENAI_API_KEY"] = getpass.getpass()
+
+from langchain_openai import ChatOpenAI
+
+from langchain_core.messages import HumanMessage
+
+model = ChatOpenAI(model="gpt-3.5-turbo")
+
+model.invoke([HumanMessage(content="Hi! I'm Bob")])
+
+'''
---
+++ backend/LangSmith.sh
@@ -0,0 +1,4 @@
+export LANGCHAIN_TRACING_V2="true"
+export LANGCHAIN_API_KEY="..."
+
+python3 -u  LangSmith.py
