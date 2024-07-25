bc014929336eb30b3aca03e5cd974e2e60d2e504
Ngai Lam Ho <nxh912@gmail.com>
2024-07-26 00:15:14 +0800

    init javascript code for input

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # chatbot
 chatbot
+
+## Overview
+You have been tasked to create an MVP application to facilitate the utilization of LLMs. The
+MVP comprises:
+
+1. Task 1: A backend component that interacts with the LLMs
+1. Task 2: A frontend component that presents LLM data to the users
+
+## Assignment Requirements
+
+### 1. You can choose to submit either Task 1 or Task 2; additional consideration will be given
+to candidates who complete both tasks.
+
+### 2. You must use Git for version control. Submit your solution through a Git repository of
+your choice and share the repository link with us.
+
+### 3. Do show the progress of your work via atomic Git commitments
+
+### 4. You must provide a README.md containing instructions on how to build and run your
+solutions locally.
+
+### 5. You must demonstrate good engineering practices in developing the app. Eg. TDD.
+
+## Installation
+
+ - pip3  install  -r Requirement.txt 
+
+ - 
--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,95 @@
-#app.py
+conn, cursor = 1, 1
+
+#Final app.py 
 #import files
 from flask import Flask, render_template, request
+import openai
+import tempfile
+import sqlite3
+import calendar
+import datetime
+ 
+from openai import OpenAI
+
+
+def init_sqlite( sqlfile) : 
+    con = sqlite3.connect( sqlfile)
+    cursor = con.cursor()
+    conn = con
+    assert(conn)
+    assert(cursor)
+
+    #Doping EMPLOYEE table if already exists.
+    cursor.execute("DROP TABLE IF EXISTS ChatLog")
+
+    #Creating table as per requirement
+    sql ='''CREATE TABLE ChatLog(
+        who CHAR(10) NOT NULL,
+        message CHAR(256) NOT NULL,
+        datetime INT  NOT NULL
+    )'''
+    cursor.execute( sql)
+    assert ( conn)
+    return conn
+
+def init_client():
+    sqlfile = tempfile.NamedTemporaryFile(mode='w+b', suffix=".sqlite", dir=None)                          
+    print(f"logfile : {sqlfile.name}")
+
+    conn = init_sqlite( sqlfile.name)
+    print(f"##   logfile : {sqlfile.name}")
+    print(f"##   conn    : {conn}")
+    assert( conn)
+    assert( conn.cursor)
+
+    client = OpenAI(
+        # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key="private",
+    )
+    return { "openai_client": client, 'sqlite_conn': conn, 'sqlite_cursor': conn.cursor}
+
+res = init_client()
+
+print(f"LINE 53, openai_client : {res['openai_client']}")
+print(f"LINE 54, sqlite_conn   : {res['sqlite_conn']}")
+print(f"LINE 54, sqlite_cursor : {res['sqlite_cursor']}")
+
+def get_completion( prompt, model="gpt-3.5-turbo"):
+    print(f"DEBUG: get_completion : '{prompt}'")
+    messages = [{"role": "user", "content": prompt}]
+
+    date = datetime.datetime.utcnow()
+    utc_time = calendar.timegm(date.utctimetuple())
+
+    sql = f" INSERT INTO ChatLog(who, message, datetime) VALUES ( 'user', '{prompt}', '{utc_time}');";
+    print(f" SQL : {sql}")
+
+    print(f"LINE_70 : DEBUG  openai.ChatCompletion.create : '{openai.ChatCompletion.create}'")
+    print(f"LINE_71 : DEBUG  res : {res}")
+
+    client = res['openai_client']
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message["content"]
+
+
 app = Flask(__name__)
+openai.api_key  = "<place your openai_api_key>"
 
 @app.route("/")
-def home():
-    return "Hello, This is Flask Application"
+def home():    
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg')
+    print(f"userText : '{userText}'")
+    response = get_completion( userText)  
+    #return str(bot.get_response(userText)) 
+    return response
+
+
+if __name__ == "__main__":  
+    app.run(debug=True)
