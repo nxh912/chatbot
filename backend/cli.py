2fad93e6d4f7e2dced2e4141e6226fd126c49b02
Ngai Lam Ho <nxh912@gmail.com>
2024-07-27 16:13:59 +0800

    using FastAPI

--- backend/cli.py
+++ backend/cli.py
@@ -1,6 +1,10 @@
 import argparse, os
+import requests, urllib.parse, json
+
 from pathlib import Path
 from openai import OpenAI
+
+ChatBotAPI='http://127.0.0.1:8000/chat'
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--llm", default='gpt-3.5-turbo')
@@ -13,18 +17,31 @@ def main():
     user_inputs = []
     llm_outputs = []
     api_key = os.environ.get("OPENAI_API_KEY")
+    assert( api_key)
     client = OpenAI( api_key=api_key )
-    #print( "client : ", client)
 
     while True:
         print("Enter text to Bot >>> ", end=""),
         user_input = input()
         if not(user_input): break
 
-        response = client.chat.completions.create(
-            model = args.llm,
-            messages = [{ "role": "system", "content": user_input }]
-        )
+        #query = {'query': "good morning", 'model': "gpt-3.5-turbo-16k"}
+
+        MODEL="gpt-3.5-turbo"
+        querystr=f"query={urllib.parse.quote(user_input)}&model={urllib.parse.quote(MODEL)}"
+        print(f"ChatBotAPI = {ChatBotAPI}")
+        print(f"querystr = {querystr}")
+        print(f"Q : {ChatBotAPI}?{querystr}")
+        response = requests.get( url=f"{ChatBotAPI}?{querystr}")
+        print( response.json())
+        assert(0)
+
+
+        response = requests.post( url="http://127.0.0.1:8000/chat", json=params)
+        print( response.json() )
+        #response = requests.post( ChatBotAPI, json=query)
+
+        assert(0)
 
         if response and response.choices and response.choices[0]:
             content = response.choices[0].message.content
