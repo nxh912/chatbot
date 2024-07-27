c57e3b75a884c69333958484520b704b9ec5575e
Ngai Lam Ho <nxh912@gmail.com>
2024-07-28 01:20:14 +0800

    added temperature option to LLM

--- backend/ChatBotREST.py
+++ backend/ChatBotREST.py
@@ -26,20 +26,23 @@ app.add_middleware(
 
 @app.get("/chat")
 @app.post("/chat")
-async def chat( query: str, model: str | None = None):
+async def chat( query: str, model: str, temperature: float):
     if not( bool(model)): model = "gpt-3.5-turbo"
     #if model not in [ "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-1106", "gpt-4o-mini"]:
     #    model = "gpt-3.5-turbo"
-    print("client : ", client)
-    print("llmmodel : ", model)
+    print("client      : ", client)
+    print("llmmodel    : ", model)
+    print("temperature : ", temperature)
 
     response = client.chat.completions.create(
         model = model,
-        messages = [{ "role": "system", "content": query }]
+        messages = [{ "role": "system", "content": query }],
     )
 
     if response and response.choices and response.choices[0]:
         content = response.choices[0].message.content
-        return {"llmmodel": model, "query":query, "content": content }
+        return { "llmmodel":    model,
+                 "query":       query,
+                 "content":     content}
     else:
-        return {"llmmodel": model, "query":query, "content": "" }
+        return {"llmmodel": model, "query":query, "content": ""}
